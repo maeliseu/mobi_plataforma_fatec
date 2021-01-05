141f37a7f4813fccbaa7130a711a0d2d32d89b50
maeliseu <maeliseu@gmail.com>
2021-01-05 17:57:24 -0300

    Clean imports in tests

--- test/app/modules/home/subpages/squad/squad_controller_test.dart
+++ test/app/modules/home/subpages/squad/squad_controller_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_modular/flutter_modular_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mobi_plataforma_fatec/app/modules/home/subpages/squad/squad_controller.dart';
 import 'package:mobi_plataforma_fatec/app/modules/home/home_module.dart';
 
 void main() {
--- test/app/modules/home/subpages/squad/squad_page_test.dart
+++ test/app/modules/home/subpages/squad/squad_page_test.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_modular/flutter_modular_test.dart';
-
-import 'package:mobi_plataforma_fatec/app/modules/home/subpages/squad/squad_page.dart';
 
 void main() {
   testWidgets('SquadPage has title', (tester) async {
